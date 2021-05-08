f71fd5103ce5ecd58d47558a918da171ae6ef407
Michael Peters <michaelpeterswa@icloud.com>
2021-05-07 17:23:41 -0700

    feat: added react-icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,15 @@
 
 import React from "react"
 import Photo from "../components/Photo"
+import { FaBeer } from 'react-icons/fa';
 
 export default function Card() {
     return (
     <div className="grid grid-cols-1 justify-center bg-white rounded-lg">
         <Photo className="self-center"></Photo>
-        <h1 className="px-2 font-sans text-3xl">Michael Peters</h1>
+        <div>
+            <h1 className="px-2 font-sans text-3xl inline-block py-2">Michael Peters <FaBeer className="inline-block align-bottom"/></h1>
+        </div>
     </div>
   )
 }
