e1d4f5881648a37fb0ab4aab1ddc5c40258dbf05
Michael Peters <michael@michaelpeterswa.com>
2021-05-08 21:44:43 -0700

    fix: url in photo component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 export default function Photo(props) {
     return (
     <div className="flex flex-wrap justify-center p-2">
-        <StaticImage src="https://raw.githubusercontent.com/michaelpeterswa/cv2/main/src/images/profile.jpg" alt="michaelpeterswa" className="rounded-full border-4 border-gray-600 antialiased" width={150}
+        <img src="https://raw.githubusercontent.com/michaelpeterswa/cv2/main/src/images/profile.jpg" alt="michaelpeterswa" className="rounded-full border-4 border-gray-600 antialiased" width={150}
       height={150}/>
     </div>
   )
