610b7a77480a17b018c39f943d9a56a657e8eb7e
Michael Peters <michael@michaelpeterswa.com>
2021-05-09 13:16:33 -0700

    feat: added conditions to weather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,17 +6,19 @@ export default function Weather(props){
     const key = "cbf5a6825f08e9ea58070102dd2d58f8"
 
     const [temp, setTemp] = useState(0)
+    const [condition, setCondition] = useState("Conditions")
     useEffect(() => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=Spokane&units=imperial&appid=${key}`)
         .then(response => response.json()) // parse JSON from request
         .then(resultData => {
             setTemp(resultData.main.temp)
+            setCondition(resultData.weather[0].main)
         }) // set data for the temperature
     }, [])
 
     return (
         <div>
-            <h3 className="px-2 font-sans text-gray-500 text-md text-center">Spokane - {temp}°F</h3>
+            <h3 className="px-2 font-sans text-gray-500 text-md text-center">Spokane - {condition} - {temp}°F</h3>
         </div>
     )
 }
