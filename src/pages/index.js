77c127c7b49b69efbe42dc38db86258891c7bcc4
Michael Peters <michael@michaelpeterswa.com>
2021-05-08 22:17:15 -0700

    fix: I broke something... it's fixed now

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import * as React from "react"
 import Card from "../components/Card"
 
 const IndexPage = () => (
-  <div class="bg-gradient-to-tl from-pink-500 via-red-500 to-yellow-500">
-    <div class="flex justify-center h-screen">
-      <div class="m-auto">
+  <div className="bg-gradient-to-tl from-pink-500 via-red-500 to-yellow-500">
+    <div className="flex justify-center h-screen">
+      <div className="m-auto">
         <Card></Card>
       </div>
     </div>
